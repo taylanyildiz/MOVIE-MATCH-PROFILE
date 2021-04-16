bd91775489e75ffed4cde674e740430d3a360e5f
Taylan YILDIZ <taylanyildz@outlook.com>
2021-04-16 10:50:38 +0300

    some chages

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,12 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: ProfileScreen());
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProfileScreen(),
+    );
   }
 }
